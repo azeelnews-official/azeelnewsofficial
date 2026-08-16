7e57eca519d2aeadb0caea9525642c4e92562ea1
ALOK KUMAR <azeelnews@gmail.com>
2026-08-16 08:19:53 +0530

    Fix Google News sitemap articles

--- app/news-sitemap.xml/route.ts
+++ app/news-sitemap.xml/route.ts
@@ -1,24 +1,21 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-const SITE_URL="https://www.azeelnews.in";
+export const dynamic = "force-dynamic";
 
 export async function GET(){
 
 const posts = await prisma.post.findMany({
 
 where:{
-status:"PUBLISHED",
-publishedAt:{
-gte:new Date(Date.now()-48*60*60*1000)
-}
+status:"PUBLISHED"
 },
 
 orderBy:{
 publishedAt:"desc"
 },
 
-take:100,
+take:50,
 
 select:{
 slug:true,
@@ -29,26 +26,45 @@ publishedAt:true
 });
 
 
-const urls = posts.map(post=>`
+const urls = posts.map((post)=>`
 
 <url>
-<loc>${SITE_URL}/article/${post.slug}</loc>
+
+<loc>
+https://www.azeelnews.in/article/${post.slug}
+</loc>
+
 <news:news>
+
 <news:publication>
-<news:name>AZEEL NEWS</news:name>
+
+<news:name>Azeel News</news:name>
+
 <news:language>en</news:language>
+
 </news:publication>
-<news:publication_date>${post.publishedAt?.toISOString()}</news:publication_date>
-<news:title><![CDATA[${post.headline}]]></news:title>
+
+
+<news:publication_date>
+${new Date(post.publishedAt ?? new Date()).toISOString()}
+</news:publication_date>
+
+
+<news:title>
+${post.headline.replace(/&/g,"&amp;")}
+</news:title>
+
+
 </news:news>
+
 </url>
 
 `).join("");
 
 
-const xml=`<?xml version="1.0" encoding="UTF-8"?>
+const xml = `<?xml version="1.0" encoding="UTF-8"?>
 
-<urlset 
+<urlset
 xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
 xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
 
